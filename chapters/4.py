fa6e59045053c3a071c77020fe6ab0f23189c71b
hlmeerichs <77294486+hlmeerichs@users.noreply.github.com>
2021-01-21 19:35:14 +0100

    Update 4.py

--- chapters/4.py
+++ chapters/4.py
@@ -25,46 +25,53 @@
 # for i in range(4): 
 #    print('four!')
 
-
 # 4.3
 
 import turtle
 import math
 
+t = turtle.Turtle() # for clean code, define this here and not later
+t.shape("arrow") # -""-
+
 # exercise 1 and 2
 def square(t, length):
-    t = turtle.Turtle()
-    t.shape("classic")
     for y in range(4):
         t.backward(length)
         t.lt(90)
 
-square("i", 75)
+square(t, 75)
 
 # exercise 3
 def polygon(t, length, n):
-    t = turtle.Turtle()
-    t.shape("classic")
+    angle = 360 / n
     for y in range(n):
         t.backward(length)
-        t.lt(360 / n)
+        t.lt(angle)
 
-polygon("o", 75, 9)
+alice = turtle.Turtle
+polygon(t, length=75, n=9) # they are called keyword arguments
 
 # exercise 4
-def circle(t, r):
+def circle(t, r): # circle with radius r
     circumference = 2 * math.pi * r
-    n = 100 
-    length = circumference / n 
-    polygon(t, length, n)
+    n = int(circumference / 3) + 3 # adding 3 guarantees that the polygon has at least three sides
+    length = circumference / n # small enough to make the circle look good, but big enough to be efficient
+    polygon(t, length, n) # to not clutter up the interface, it is better to choose assign an appropriate value to the local variable n
 
-circle("t", 190) 
+circle(t, 190) 
 
 # exercise 5
-def arc(r, angle):
-    angle = 
-    circle("a", 190)
-    
+def arc(t, r, angle):
+    arc_length = 2 * math.pi * r * angle / 360 # meaning?????
+    n = int(arc_length / 3) + 1
+    step_length = arc_length / n
+    step_angle = angle / n
 
+    for i in range(n):
+        t.fd(step_length)
+        t.lt(step_angle)
+
+arc(t, 90, 90)
 
 turtle.mainloop()
+
