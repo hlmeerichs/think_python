08eccbecbbd879fc9641c98e1836d7d3ce7c9427
hlmeerichs <77294486+hlmeerichs@users.noreply.github.com>
2021-02-06 19:57:28 +0100

    formated

--- exercise/pie.py
+++ exercise/pie.py
@@ -6,22 +6,26 @@ import math
 
 t = turtle.Turtle()
 
+
 def polygon(t, length, n):
     angle = 360 / n
     for y in range(n):
         t.backward(length)
         t.lt(angle)
 
+
 def triangle(t, length):
     for y in range(3):
         t.backward(length)
         t.lt(120)
+
 
 def pie(t, n, length):
     p = n * 2
     for i in range(p):
         triangle(t, length)
         t.lt(360 / p)
+
 
 pie(t, 3, 60)
 
